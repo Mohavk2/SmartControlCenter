32d9e7cf8f74974ba6d9fbaeb8c0b6e3b6fe04fb
Mohavk <mohavkv12@gmail.com>
2020-11-25 12:16:41 +0200

    Add. Plugin assamblies loader. Some project configs.

---
+++ HostWebUI/PluginLoader.cs
@@ -0,0 +1,42 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace HostWebUI
+{
+    public static class PluginLoader
+    {
+        private static string pluginDirectoryPath = @"C:\Users\Saint\source\repos\SmartControlCenter\HostWebUI\bin\Debug\net5.0\Plugins\net5.0";
+
+        public static List<Assembly> GetPluginAssamblies()
+        {
+            string [] pluginsPaths = Directory.GetFiles(pluginDirectoryPath, "*.dll");
+            List<Assembly> pluginAssemblies = new List<Assembly>();
+
+            foreach(var path in pluginsPaths)
+            {
+                Assembly assembly = Assembly.LoadFrom(path);
+                if(assembly.ContainsPlugin(typeof(IPlugin)))
+                    pluginAssemblies.Add(assembly);
+            }
+            return pluginAssemblies;
+        }
+
+        static bool ContainsPlugin(this Assembly assembly, Type pluginType)
+        {
+            Type[] types = assembly.GetTypes();
+            foreach(var type in types)
+            {
+                if(type.IsAssignableTo(pluginType))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
